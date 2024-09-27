33e8796162ad8d3f0cbaba72bee5303aab26e24e
Mg <135870538+Simo-Mg@users.noreply.github.com>
2024-09-27 16:40:33 +0100

    Add files via upload

---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaghri <mmaghri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 12:15:02 by mmaghri           #+#    #+#             */
+/*   Updated: 2024/09/27 16:34:56 by mmaghri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+class ScalarConverter {
+    private :
+        ScalarConverter();
+    public :
+        ScalarConverter(const ScalarConverter&value);
+        ScalarConverter& operator= (const ScalarConverter& target);
+        ~ScalarConverter();
+        static void convert(const std::string value);
+};
