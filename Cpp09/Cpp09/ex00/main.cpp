c9e961e0d4070f7449d3db0931bbf980a3fbdadd
Mg <135870538+Mohammed-Maghri@users.noreply.github.com>
2024-11-11 21:10:51 +0100

    Fill Form !

--- Cpp09/Cpp09/ex00/main.cpp
+++ Cpp09/Cpp09/ex00/main.cpp
@@ -27,16 +27,17 @@ void functionCheckWithExactDates (std::string values, StoreElements Data) {
     std::map<std::string, double>::iterator end = Data.theend();
     std::string startOfVALUE = returnWithoutSpaces(values.substr(0, holdstart + 1));
     startOfVALUE = startOfVALUE.substr(startOfVALUE.find_first_not_of(" \t\n"), startOfVALUE.find_last_not_of(" \t\n") + 1);
-    std::string endOfVALUE   = returnWithoutSpaces(values.substr(holdstart));
+    std::string endOfVALUE = returnWithoutSpaces(values.substr(holdstart + 1));
+    endOfVALUE = endOfVALUE.substr(endOfVALUE.find_first_not_of(" \t\n"), endOfVALUE.find_last_not_of(" \t\n") + 1);
     end-- ;
     if (end->first <= startOfVALUE){
-        std::cout << startOfVALUE << " ->" << " ( " << end->first << " ) "<< std::endl ;
+        std::cout << startOfVALUE << " => " << endOfVALUE << " " << (atof(endOfVALUE.c_str()) * end->second) << std::endl;
         return ;
     }
     for (; start != end; start++){
         if (start->first >= startOfVALUE){
-            std::cout << startOfVALUE << " ==> "<< start->first << std::endl;
-            std::cout << std::endl ;
+            std::cout << startOfVALUE << " => " << endOfVALUE << " " << (atof(endOfVALUE.c_str()) * start->second) << std::endl;
+            break ;
         }
     };
 };
@@ -50,45 +51,11 @@ int main(){
     std::map<std::string, int>::iterator startIterating = Values.postionStart();
     std::map<std::string, int>::iterator endIterating = Values.positionEnd();
     for (; startIterating != endIterating; startIterating++){
-        functionCheckWithExactDates(startIterating->first, Values);
+        if (ParsDays(startIterating->first).find("Error") == std::string::npos)
+            functionCheckWithExactDates(startIterating->first, Values);
+        else {
+            std::cout << ParsDays(startIterating->first) << std::endl ;
+        }
     };
-    // for (; itis != itsend; itis++) {
-    //     CheckStore = FunctionGetElmenetDates(*itis, "|");
-    //     std::cout << *itis << std::endl ;
-    //     it = Values.beging();
-    //     itend = Values.theend() ;
-    //     for (; it != itend ; it++){
-    //         if (CheckStore.find("Error") == std::string::npos){
-    //             StoreInaelement = FunctionGetElmenetDates(it->first, ",");
-    //             if (CheckStore > endthis->first){
-    //                 std::cout << CheckStore << " Exact date "  << endthis->first << " ";
-    //                 std::cout << endthis->second << std::endl ;
-    //                 break;
-    //             }
-    //             if (startthis->first > CheckStore) {
-    //                 std::cout << "No Existing Dates " << *itis  ;
-    //                 break ;
-    //             }
-    //             else if (CheckStore <= StoreInaelement){
-    //                 if (it != Values.beging() && CheckStore != StoreInaelement) {
-    //                     it--; flag = 1; std::cout << " |" << it->first << "| " << std::endl;
-    //                     std::cout << "Price Was this Much -> " << it->second << std::endl ;
-    //                 }
-    //                 else {
-    //                     flag = 1 ; 
-    //                     std::cout << it->first <<  " => " ;
-    //                     std::cout << it->second << " = "  << std::endl ;
-    //                 }
-    //                 break ;
-    //             }
-    //         }
-    //         else {
-    //             std::cout << *itis << std::endl ;
-    //             break ;
-    //         }
-    //         flag = 0 ;
-    //     };
-    //     std::cout << std::endl ;
-    // }
     return (0);
 };
